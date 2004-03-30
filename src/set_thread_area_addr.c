7fcf9e473e7c7156c73c249014651625ecc5e6f1
Valery Reznic <valery_reznic@yahoo.com>
2004-03-30 11:11:28 +0000

    Change fd in the running process to something more safe (it's only needed indeed in case when I somehow miss set_thread_area syscall. )

--- src/set_thread_area_addr.c
+++ src/set_thread_area_addr.c
@@ -19,6 +19,8 @@
 #include <sys/ptrace.h>
 #include <sys/reg.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <sys/wait.h>
 #include <sys/syscall.h>
 
@@ -138,6 +140,37 @@ void do_work(const char *name, const char *process, const pid_t child)
 	}	
 }	
 
+static void safe_fd()
+{
+	/* 
+	 * If I somehow miss 'set_thread_area' syscall
+	 * inspected process will continue execution.
+	 * In this case I want following:
+	 * stdin  - close. (I don't ant interactive program sleep forever)
+	 * stdout - redirected to /dev/null
+	 * stderr - leave it as is. I want be able to see
+	 *          possible eror from run-time-loader
+	 * NOTE: solution with stdin is not perfect.
+	 * if same program expect for example to get exactly 'yes' or 'no'
+	 * closed stdin is no help.
+	 * May be I need add timeout and try to kill application after that ?
+	 */
+
+	/* 
+	 * In case of error I not give messages here,
+	 * because inspected process should be stopped before it
+	 * use these file descriptors, so I want to avoid this
+	 * (possible) noise
+	 */
+	int new_stdout;
+	new_stdout = open("/dev/null", O_WRONLY);
+	if (new_stdout >= 0) {
+		dup2(new_stdout, STDOUT_FILENO);
+		close(new_stdout);
+	}
+	close(STDIN_FILENO); 
+}
+
 int main(int argc, char *argv[], char *envp[])
 {
 	pid_t child;
@@ -158,6 +191,7 @@ int main(int argc, char *argv[], char *envp[])
 				);
 				exit(1);
 			}
+			safe_fd();
 			execve(argv[1], &argv[1], envp);
 			fprintf(
 				stderr,
