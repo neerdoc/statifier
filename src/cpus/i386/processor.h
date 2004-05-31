6f44c64f5f08e84312f800ce8dd37cbaff6578f8
valery_reznic <valery_reznic@13c1b854-16e0-4214-857e-bfa83f13b894>
2004-05-31 08:13:43 +0000

    Added Files:  	cpus/i386/processor.h cpus/x86_64/processor.h Removed Files:  	processor.h
    processor.h was splitted to */processor.h
    REG_SIZE was removed
    
    
    git-svn-id: svn://svn.code.sf.net/p/statifier/code/statifier/trunk@204 13c1b854-16e0-4214-857e-bfa83f13b894

--- src/cpus/i386/processor.h
+++ src/cpus/i386/processor.h
@@ -7,31 +7,15 @@
   See LICENSE file in the doc directory.
 */
 
-/* This file define per-processor things */
+/* This file define per-processor things for i386 */
 
 #ifndef processor_h
 #define processor_h
 
-#undef REGISTER_SIZE
-#ifdef __i386__
 	#define REGISTER_SIZE 4
 	#define SYSCALL_REG   (ORIG_EAX)
 	#define PC_REG        (EIP)
 	#define PC_OFFSET_AFTER_SYSCALL 2
-#endif
-
-#ifdef __x86_64__
-	#define REGISTER_SIZE 8
-	#define SYSCALL_REG   (ORIG_RAX)
-	#define PC_REG        (RIP)
-	#define PC_OFFSET_AFTER_SYSCALL 2
-#endif
-
-#ifndef REGISTER_SIZE
-   #error This processor not supported (yet)
-#endif
-
-#define REG_SIZE REGISTER_SIZE
 
 #endif /* processor_h */
 
