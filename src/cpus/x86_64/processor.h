1d0b81cae1150a5087d376ae259b2e8e062f9fb1
valery_reznic <valery_reznic@13c1b854-16e0-4214-857e-bfa83f13b894>
2004-06-06 16:21:23 +0000

    Comments converted from # to /* */
    git-svn-id: svn://svn.code.sf.net/p/statifier/code/statifier/trunk@226 13c1b854-16e0-4214-857e-bfa83f13b894

--- src/cpus/x86_64/processor.h
+++ src/cpus/x86_64/processor.h
@@ -1,11 +1,11 @@
 /*
-  Copyright (C) 2004 Valery Reznic
-  This file is part of the Elf Statifier project
-  
-  This project is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License.
-  See LICENSE file in the doc directory.
-*/
+ * Copyright (C) 2004 Valery Reznic
+ * This file is part of the Elf Statifier project
+ * 
+ * This project is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License.
+ * See LICENSE file in the doc directory.
+ */
 
 /* This file define per-processor things for x86_84 (amd) */
 
