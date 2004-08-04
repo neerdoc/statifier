72ee46cdfac4ddce84f70e629f9191d241c7d0c3
Valery Reznic <valery_reznic@yahoo.com>
2004-08-04 14:51:14 +0000

    Modified Files:  	Makefile - added fep.c and fep.sh Added Files:  	fep.c fep.sh  - Find (Starter) Entry Point

---
+++ src/fep.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# Copyright (C) 2004 Valery Reznic
+# This file is part of the Elf Statifier project
+# 
+# This project is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License.
+# See LICENSE file in the doc directory.
+
+# This script try to find place for starter and print
+# starter's entry point to the stdout
+
+[ $# -ne 2 -o "x$1" = "x" -o "x$2" = "x" ] && {
+	echo "Usage: $0 <map_file> <starter>" 1>&2
+	exit 1
+}
+
+MapFile=$1
+Starter=$2
+D=`dirname $0` || exit
+
+# Get from the MapFile peers "FileName StartAddr" 
+FileAddr="`awk '
+	BEGIN {
+		ind = 0;
+	}
+	{
+		Start = $1
+		Name = $5
+		if (Name != "") {
+			if ( !(Name in Names)) {
+				Names[Name] = ""
+				Output[ind++] = Name " " Start
+			}
+		}
+	}
+	END {
+		for (i = 0; i < ind; i++) print Output[i];	
+	}
+' < $MapFile
+`" || exit
+
+set -- $FileAddr
+[ $# -eq 0 ] && {
+	echo "$0: nothing found in the '$MapFile'" 1>&2
+	exit 1
+}
+
+while [ $# -ne 0 ]; do
+	File=$1;  shift || exit
+	Start=$1; shift || exit
+	Entry=`$D/fep $File $Start $Starter` || exit
+done || exit
+
+[ "x$Entry" = "x" ] && {
+	echo "$0: Can't find room for the starter" 1>&2
+	exit 1
+}
+
+echo $Entry || exit
+exit 0
