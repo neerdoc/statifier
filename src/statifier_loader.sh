d56b93c20ccf8ea74814fb9210012e5b113c98d5
valery_reznic <valery_reznic@13c1b854-16e0-4214-857e-bfa83f13b894>
2005-01-23 16:35:15 +0000

    GetSymbol functionupdated to take third argument - IsGlobal. If it set to "yes", we are looking to this symbol only in the symtab, doesn't matter loader stripped or not. All usage of this function got third argument too.
    git-svn-id: svn://svn.code.sf.net/p/statifier/code/statifier/trunk@411 13c1b854-16e0-4214-857e-bfa83f13b894

--- src/statifier_loader.sh
+++ src/statifier_loader.sh
@@ -26,19 +26,21 @@ function GetSymbol
 	#       value '0x0' will be prined and return status 0.
 	# 2. Symbol WAS FOUND.
 	#    return status - 0  and it's value printed out
-	[ $# -ne 2 -o "x$1" = "x" -o "x$2" = "x" ] && {
-		Echo "$0: Usage: GetSymbol_Symtab <Symbol> <IsMandatory>"
+	[ $# -ne 3 -o "x$1" = "x" -o "x$2" = "x" -o "x$3" = "x" ] && {
+		Echo "$0: Usage: GetSymbol <Symbol> <IsMandatory> <IsGlobal>"
 		return 1
 	}
 	local Symbol=$1
 	local IsMandatory=$2
+	local IsGlobal="$3"
 
 	local MsgNotFound="$0: Symbol '$Symbol' not found in the interpreter '$val_interpreter'"
 	local MsgNoIdea="$0: internal error: no idea how to find '$Symbol' in the interpreter '$val_interpreter' without symtab."
 
 	local Value
-	if [ "X$val_interpreter_has_symtab" = "Xyes" ]; then
-		# Interpreter has symtab. Good. Just try to find symbol in.
+	if [ "X$val_interpreter_has_symtab" = "Xyes" -o "X$IsGlobal" = "Xyes" ]; then
+		# Interpreter has symtab. or symbol is global.
+		# Good. Just try to find symbol in.
 		Value=`awk -vSymbol="$Symbol" '{
 			if ($NF == Symbol) {
 				print "0x" $2; 
@@ -106,8 +108,8 @@ function ForDump
 {
 	local val_breakpoint_start
 
-	val_breakpoint_start=`GetSymbol _dl_start_user 1`   || return
-	echo "val_breakpoint_start='$val_breakpoint_start'" || return
+	val_breakpoint_start=`GetSymbol _dl_start_user 1 no` || return
+	echo "val_breakpoint_start='$val_breakpoint_start'"  || return
 	return 0
 }
 
@@ -118,38 +120,38 @@ function ForStarter
 	local val_dl_list=""
 
 	Var="_dl_argc"
-	Value=`GetSymbol $Var 1` || return
-	echo "# $Var=$Value"     || return
+	Value=`GetSymbol $Var 1 no` || return
+	echo "val_$Var=$Value"      || return
 	val_dl_list="$val_dl_list $Value"
 
 	Var="_dl_argv"
-	Value=`GetSymbol $Var 1` || return
-	echo "# $Var=$Value"     || return
+	Value=`GetSymbol $Var 1 no` || return
+	echo "val_$Var=$Value"      || return
 	val_dl_list="$val_dl_list $Value"
 
 	Var="_environ"
-	Value=`GetSymbol $Var 1` || return
-	echo "# $Var=$Value"     || return
+	Value=`GetSymbol $Var 1 no` || return
+	echo "val_$Var=$Value"      || return
 	val_dl_list="$val_dl_list $Value"
 
 	Var="_dl_auxv"
-	Value=`GetSymbol $Var 1` || return
-	echo "# $Var=$Value"     || return
+	Value=`GetSymbol $Var 1 no` || return
+	echo "val_$Var=$Value"      || return
 	val_dl_list="$val_dl_list $Value"
 
 	Var="_dl_platform"
-	Value=`GetSymbol $Var 0` || return
-	echo "# $Var=$Value"     || return
+	Value=`GetSymbol $Var 0 no` || return
+	echo "val_$Var=$Value"      || return
 	val_dl_list="$val_dl_list $Value"
 
 	Var="_dl_platformlen"
-	Value=`GetSymbol $Var 0` || return
-	echo "# $Var=$Value"     || return
+	Value=`GetSymbol $Var 0 no` || return
+	echo "val_$Var=$Value"      || return
 	val_dl_list="$val_dl_list $Value"
 
 	Var="__libc_stack_end"
-	Value=`GetSymbol $Var 0` || return
-	echo "# $Var=$Value"     || return
+	Value=`GetSymbol $Var 0 yes` || return
+	echo "val_$Var=$Value"       || return
 	val_dl_list="$val_dl_list $Value"
 
 	echo "val_dl_list='$val_dl_list'" || return
