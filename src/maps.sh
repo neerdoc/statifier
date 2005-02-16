b8b0e9e130baaf669d241244029cca80852c3ae9
valery_reznic <valery_reznic@13c1b854-16e0-4214-857e-bfa83f13b894>
2005-02-16 08:15:10 +0000

    Adopted for amd64 2.6.9, where vsyscall can't be dumped. Added third parameter - uname_m
    git-svn-id: svn://svn.code.sf.net/p/statifier/code/statifier/trunk@419 13c1b854-16e0-4214-857e-bfa83f13b894

--- src/maps.sh
+++ src/maps.sh
@@ -7,12 +7,13 @@
 # modify it under the terms of the GNU General Public License.
 # See LICENSE file in the doc directory.
 
-[ $# -ne 2 -o "x$1" = "x" -o "x$2" = "x" ] && {
-	echo "Usage: $0 <process_file> <maps_file>" 1>&2
+[ $# -ne 3 -o "x$1" = "x" -o "x$2" = "x" -o "x$3" = "x" ] && {
+	echo "Usage: $0 <process_file> <maps_file> <uname_m>" 1>&2
 	exit 1
 }
 process_file=$1
 maps_file=$2
+uname_m=$3
 
 # Process file is output from gdb's info process and
 # has following format: (without #)
@@ -25,13 +26,20 @@ maps_file=$2
 read dummy1 pid dummy2 < $process_file || exit
 rm -f $maps_file || exit
 
-awk '{
+awk -v uname_m="$uname_m" '{
 	StartStop  = $1
 	Permission = $2
 	Offset     = $3
 	Device     = $4
 	Inode      = $5
 	Name       = $6
+	if (uname_m == "x86_64") {
+		# (at least) linux 2.6.9 on amd64 
+		# (2.6.9-1.667smp) has vsyscall area,
+		# which can`t be dumped by gdb.
+		# So, let us skip it (anyway i don`t need it)
+        	if (StartStop == "ffffffffff600000-ffffffffffe00000") next;
+	}
 	split(StartStop, Array, "-");
 	printf "0x%s 0x%s %s 0x%s %s\n", Array[1], Array[2], Permission, Offset, Name
 }' < /proc/$pid/maps > $maps_file || exit
