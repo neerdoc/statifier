64f60e956cf1bf2a97ee3750dccb6ce05d09889e
valery_reznic <valery_reznic@13c1b854-16e0-4214-857e-bfa83f13b894>
2010-02-16 10:02:11 +0000

    adopted to use my_gdb and it's output instead of gdb. TODO - environment variables
    git-svn-id: svn://svn.code.sf.net/p/statifier/code/statifier/trunk@558 13c1b854-16e0-4214-857e-bfa83f13b894

--- src/maps.sh
+++ src/maps.sh
@@ -7,40 +7,45 @@
 # modify it under the terms of the GNU General Public License.
 # See LICENSE file in the doc directory.
 
-[ $# -ne 3 -o "x$1" = "x" -o "x$2" = "x" -o "x$3" = "x" ] && {
-	echo "Usage: $0 <process_file> <maps_file> <uname_m>" 1>&2
+[ $# -ne 4 -o "x$1" = "x" -o "x$2" = "x" -o "x$3" = "x" -o "x$4" = "x" ] && {
+	echo "Usage: $0 <uname_m> <pid> <output> <output_all_mappings>" 1>&2
 	exit 1
 }
-process_file=$1
-maps_file=$2
-uname_m=$3
+uname_m=$1
+pid="$2"
+output="$3"
+output_all_mappings="$4"
 
-# Process file is output from gdb's info process and
-# has following format: (without #)
-#process 24039
-#cmdline = '/bin/df'
-#cwd = '/home/users/valery'
-#exe = '/bin/df'
+input="/proc/$pid/maps"
+output_orig="$output".orig
 
-# Get PID of inspected file
-read dummy1 pid dummy2 < $process_file || exit
-rm -f $maps_file || exit
+cat $input > $output_orig || exit
 
-awk -v uname_m="$uname_m" '{
+awk -v uname_m="$uname_m" -v output_all_mappings="$output_all_mappings" '{
 	StartStop  = $1
 	Permission = $2
 	Offset     = $3
 	Device     = $4
 	Inode      = $5
 	Name       = $6
-	if (uname_m == "x86_64") {
-		# (at least) linux 2.6.9 on amd64 
-		# (2.6.9-1.667smp) has vsyscall area,
-		# which can`t be dumped by gdb.
-		# So, let us skip it (anyway i don`t need it)
-        	if (StartStop == "ffffffffff600000-ffffffffffe00000") next;
-	}
 	split(StartStop, Array, "-");
-	printf "0x%s 0x%s %s 0x%s %s\n", Array[1], Array[2], Permission, Offset, Name
-}' < /proc/$pid/maps > $maps_file || exit
+	Start = Array[1]
+	Stop  = Array[2]
+	if (output_all_mappings == "0") {
+		if (Permission == "---p") next
+		if (Name == "[vdso]") next
+		if (Name == "[stack]") next
+		if (Name == "[vsyscall]") next
+		if (Start == "ffffffffff600000") {
+			if (uname_m == "x86_64") {
+				# (at least) linux 2.6.9 on amd64 
+				# (2.6.9-1.667smp) has vsyscall area,
+				# which can`t be dumped by gdb.
+				# So, let us skip it (anyway i don`t need it)
+				next
+			}
+		}
+	}
+	printf "0x%s 0x%s %s 0x%s %s\n", Start, Stop, Permission, Offset, Name
+}' $output_orig > $output || exit
 exit 0
