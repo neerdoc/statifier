dedc02823867adf7e2fee54452f4324bb550e06d
valery_reznic <valery_reznic@13c1b854-16e0-4214-857e-bfa83f13b894>
2010-02-23 14:46:13 +0000

    cleanup no need in the uname and tls data anymore
    git-svn-id: svn://svn.code.sf.net/p/statifier/code/statifier/trunk@565 13c1b854-16e0-4214-857e-bfa83f13b894

--- src/statifier_common.sh
+++ src/statifier_common.sh
@@ -10,39 +10,11 @@
 # This script have to detect kernel/loader properties
 # 
 
-function CheckTls
-{
-	# Test if interpreter/kernel use TLS (thread local storage)
-	local val_has_tls="0"
-	local val_breakpoint_thread=""
-	local res
-	res="`
-		awk '{ 
-			if ($0 ~ "tls") { 
-				print "yes_probably_we_have_tls"; 
-				exit(0);
-			} 
-		}' < $LOADER_SYMBOLS
-	`" || return
-	[ "x$res" = "x" ] || {
-		val_breakpoint_thread="`$D/set_thread_area_addr $val_interpreter_file_entry $D/tls_test`" || return
-		[ \! "x$val_breakpoint_thread" = "x" ] && {
-			val_has_tls="1"
-		}
-	}
-	echo "val_has_tls='$val_has_tls'"                     || return
-	echo "val_breakpoint_thread='$val_breakpoint_thread'" || return
-	return
-}
-
 function Main
 {
 	set -e
 		source $OPTION_SRC || return
 	set +e
-
-	local val_uname_m
-	val_uname_m=`uname -m` || return
 
 	local val_interpreter
 	val_interpreter=`$D/$elf_class/elf_data -i $opt_orig_exe` || return
@@ -52,7 +24,6 @@ function Main
 	(
 		set -e
 		val_interpreter_file_entry=`$D/$elf_class/elf_data -e $val_interpreter`
-		echo "val_uname_m=$val_uname_m"
 		echo "val_elf_class=$val_elf_class"
 		echo "val_interpreter='$val_interpreter'"
 		echo "val_interpreter_file_entry='$val_interpreter_file_entry'"
@@ -62,7 +33,6 @@ function Main
                    	-W val_interpreter_file_rw_seg_addr= \
 			-S val_interpreter_rw_seg_size=      \
 		$val_interpreter
-		CheckTls
 	) || return 
 	return 0
 }
