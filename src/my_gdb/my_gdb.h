9415ac3e0d41043e610c6e3e3c4d924342b352e5
Valery Reznic <valery_reznic@yahoo.com>
2010-02-24 14:41:30 +0000

    added LICENSE note

--- src/my_gdb/my_gdb.h
+++ src/my_gdb/my_gdb.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2010 Valery Reznic
+ * This file is part of the Elf Statifier project
+ * 
+ * This project is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License.
+ * See LICENSE file in the doc directory.
+ */
+
 #ifndef my_gdb_h
 #define my_gdb_h
 
